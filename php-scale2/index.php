ae6b6132a3521746aa725e80bd18a0fbdbd1556d
GU-CL <Guenterc@gmx.net>
2025-04-25 12:32:14 +0200

    Add /php-scale2 directory with index.php

---
+++ php-scale2/index.php
@@ -0,0 +1,8 @@
+<?php
+  print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  $x = 0.0001;
+  for $i = 0; $i <= 1000000; $i++ {
+        $x = += sqrt($x);
+  }
+  echo "OK!";
+?>
