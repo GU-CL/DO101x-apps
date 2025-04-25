a2681724397ad0a8be8456d50bb56b55bdc5c1c3
GU-CL <Guenterc@gmx.net>
2025-04-25 12:46:06 +0200

    fix a typo bug

--- php-scale2/index.php
+++ php-scale2/index.php
@@ -2,7 +2,7 @@
   print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
   $x = 0.0001;
   for $i = 0; $i <= 1000000; $i++ {
-        $x = += sqrt($x);
+        $x += sqrt($x);
   }
   echo "OK!";
 ?>
