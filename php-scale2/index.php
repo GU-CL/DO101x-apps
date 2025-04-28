eb17d3e3718f5f289645b395eb4a33b3a09aec9d
GU-CL <Guenterc@gmx.net>
2025-04-28 14:56:26 +0200

    Fix missing Brakets

--- php-scale2/index.php
+++ php-scale2/index.php
@@ -1,7 +1,7 @@
 <?php
   print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
   $x = 0.0001;
-  for $i = 0; $i <= 1000000; $i++ {
+  for ($i = 0; $i <= 1000000; $i++) {
         $x += sqrt($x);
   }
   echo "OK!";
